e8022c7405518385b4106efca5922a197bf17e1c
ydvsailendar <ydvsailendar0905@gmail.com>
2020-08-27 07:51:27 +0545

    server

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import microConfig from "./mikro-orm.config";
+// import { Post } from "./entities/Post";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { __prod__ } from "./constants";
+import { MyContext } from "./types";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+        // disableTTL: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        secure: __prod__, // cookie only works in https,
+        sameSite: "lax", //csrf
+      },
+      saveUninitialized: false,
+      secret: "randomStringGenetate",
+      resave: false,
+    })
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver, UserResolver],
+      validate: false,
+    }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+  //   console.log("hello man", orm);
+  // instance of post
+  // const post = orm.em.create(Post, { title: "my first post" });
+  //save the data to post table
+  // await orm.em.persistAndFlush(post);
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
+};
+
+main().catch((error) => {
+  console.error(error);
+});
---
+++ src/resolvers/user.ts
@@ -0,0 +1,130 @@
+import {
+  Resolver,
+  InputType,
+  Field,
+  Arg,
+  Ctx,
+  Mutation,
+  ObjectType,
+  Query,
+} from "type-graphql";
+import { MyContext } from "src/types";
+import { User } from "../entities/User";
+import argon2 from "argon2";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => UserResponse)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { req, em }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Length must be greater than 2",
+          },
+        ],
+      };
+    }
+    if (options.password.length <= 3) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Length must be greater than 3",
+          },
+        ],
+      };
+    }
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    try {
+      await em.persistAndFlush(user);
+    } catch (error) {
+      if (error.code === "23505" || error.detail.includes("already exists")) {
+        //duplicate user error
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "Username already taken",
+            },
+          ],
+        };
+      }
+    }
+    // store userid session
+    req.session.userId = user.id;
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "User doesn't exist",
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Incorrect password",
+          },
+        ],
+      };
+    }
+    req.session.userId = user.id;
+    return { user };
+  }
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    if (!req.session.userId) {
+      return null;
+    }
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+}

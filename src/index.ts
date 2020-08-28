27e28485abefe344345f099645eead7f91f24a01
shailendra4 <shailendra@bottle.com.np>
2020-08-28 06:52:47 +0545

    logout mutation and constants for cookie name

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import { UserResolver } from "./resolvers/user";
 import redis from "redis";
 import session from "express-session";
 import connectRedis from "connect-redis";
-import { __prod__ } from "./constants";
+import { __prod__, COOKIE_NAME } from "./constants";
 import cors from "cors";
 
 const main = async () => {
@@ -31,7 +31,7 @@ const main = async () => {
   );
   app.use(
     session({
-      name: "qid",
+      name: COOKIE_NAME,
       store: new RedisStore({
         client: redisClient,
         disableTouch: true,
